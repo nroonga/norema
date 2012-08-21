3b314c800b3c07ff045b7691ada59378d7fafbd0
Yoji SHIDARA <dara@shidara.net>
2012-08-21 17:22:04 +0900

    Use scroll spy

--- public/js/norema.js
+++ public/js/norema.js
@@ -1,3 +1,12 @@
 $(document).ready(function($) {
+  var offset = 50;
+
   $('#query').focus();
+
+  // scroll spy
+  $('.side-nav li a').click(function(event) {
+    event.preventDefault();
+    $($(this).attr('href'))[0].scrollIntoView();
+    scrollBy(0, -offset);
+  });
 });

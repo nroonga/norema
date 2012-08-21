2c3d73478080b1e7d1be9199ae29550d1dc5af13
Yoji SHIDARA <dara@shidara.net>
2012-08-21 17:26:56 +0900

    Scroll offset control should work only on TOC

--- public/js/norema.js
+++ public/js/norema.js
@@ -4,7 +4,7 @@ $(document).ready(function($) {
   $('#query').focus();
 
   // scroll spy
-  $('.side-nav li a').click(function(event) {
+  $('.side-nav ul.toc li a').click(function(event) {
     event.preventDefault();
     $($(this).attr('href'))[0].scrollIntoView();
     scrollBy(0, -offset);
